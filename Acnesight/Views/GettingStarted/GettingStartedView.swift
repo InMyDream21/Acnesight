fa502a045a808dccce5fa2734a69067a14241d29
KewpieBinar <gedebinar23@gmail.com>
2025-06-18 11:04:34 +0700

    init Getting Started Page

---
+++ Acnesight/Views/GettingStarted/GettingStartedView.swift
@@ -0,0 +1,76 @@
+//
+//  GettingStartedView.swift
+//  Acnesight
+//
+//  Created by Gede Binar on 18/06/25.
+//
+
+import SwiftUI
+
+struct GettingStartedContent : View {
+    private let textList : [String] = [
+        "Please stand in front of good natural light",
+        "Point out the camera to your acne",
+        "Do not wear makeup or accessories"
+    ]
+    var body: some View {
+        VStack(spacing:20){
+            VStack {
+                Image("GetStartedAsset")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .frame(width: 205,height: 205)
+            .background(.white)
+            .clipShape(Ellipse())
+            .overlay(
+                Ellipse()
+                    .stroke(Color("SecondaryColor"), lineWidth: 14)
+            )
+            
+            
+            Text("Time for a selfie")
+                .font(.largeTitle)
+                .bold()
+                .foregroundStyle(.white)
+            VStack (alignment:.leading,spacing:20){
+                ForEach(textList, id: \.self) {txt in
+                    HStack(spacing:14){
+                        Image(systemName: "checkmark.seal.fill")
+                            .foregroundStyle(Color("OliveGreen"))
+                        Text(txt)
+                            .font(.footnote)
+                        
+                    }
+                }
+            }
+            //            .frame(maxWidth: .infinity)
+            .padding(.vertical,20)
+            .padding(.horizontal,24)
+            .background(.white)
+            .cornerRadius(12)
+        }
+    }
+}
+
+struct GettingStartedView: View {
+    var body: some View {
+        ZStack{
+            GettingStartedContent()
+            OnBoardingButton(
+                text: "TAKE A SELFIE",
+                onClick: {
+                    //                    hasSeenOnboarding = true
+                    //                    router.navigateToCapture()
+                },isSecondary: true)
+            .frame(maxHeight: .infinity,alignment: .bottom)
+        }
+        .padding(.horizontal,30)
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
+        .background(Color("PrimaryColor"))
+    }
+}
+
+#Preview {
+    GettingStartedView()
+}
